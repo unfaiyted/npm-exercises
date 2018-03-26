d38cb9924b0807ec62e8d145d8b1b1575705e23d
unfaiyted <unfaiyted@gmail.com>
2018-03-26 11:47:35 -0500

    adding sayHello arrow function

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+/*
+Inside of the src directory, create a file named index.js. This is the entry point of your application.
+Inside of index.js, write a function named sayHello using a arrow function, that logs hello to the console.
+Then call that function.
+ */
+
+
+const sayHello = () => console.log('Hello');
+
+sayHello();
