4fb2258280c5ff0f56e2a4647b2819d069224ec6
unfaiyted <unfaiyted@gmail.com>
2018-03-26 12:02:52 -0500

    adding watch capabilities, as well as dist source identification scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const $ = require('jquery');
 /*
 Inside of the src directory, create a file named index.js. This is the entry point of your application.
 Inside of index.js, write a function named sayHello using a arrow function, that logs hello to the console.
@@ -9,3 +9,6 @@ Then call that function.
 const sayHello = () => console.log('Hello');
 
 sayHello();
+
+
+$('#hello').text('Hello Friend NODE! JQUERY IS HERE!!!');
