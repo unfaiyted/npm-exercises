ea2b2cb94aafa9e64f35b176f13cb5ef9102cc76
unfaiyted <unfaiyted@gmail.com>
2018-03-26 12:51:42 -0500

    adding message code for include/export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const $ = require('jquery');
+const message = require('./say-hello.js');
 /*
 Inside of the src directory, create a file named index.js. This is the entry point of your application.
 Inside of index.js, write a function named sayHello using a arrow function, that logs hello to the console.
@@ -12,3 +13,5 @@ sayHello();
 
 
 $('#hello').text('Hello Friend NODE! JQUERY IS HERE!!!');
+
+console.log(message.aNiceMessage());
---
+++ src/say-hello.js
@@ -0,0 +1,10 @@
+
+
+
+const aNiceMessage = () => 'A nice message';
+const add = (x, y) => x + y;
+const subtract = (x, y) => y - x;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+module.exports = {add, subtract, multiply, divide, aNiceMessage};
